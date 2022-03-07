425616bfb32d0344bcc765a0727cb9ec54b3ae2d
csh0034 <csh0034@gmail.com>
2022-03-08 05:06:07 +0900

    chore: 의존성 정리

--- module-api/build.gradle.kts
+++ module-api/build.gradle.kts
@@ -1,6 +1,5 @@
 dependencies {
     implementation(project(":module-core"))
-    implementation("org.springframework.boot:spring-boot-starter-web")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
 
